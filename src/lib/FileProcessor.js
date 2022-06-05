6233ccd6944388e8ae0128f4148284f9211fad4d
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2022-06-05 11:44:02 +0000

    chore: uppfæra dependency
    marked úr v2->v4 braut hvernig helperar voru sóttir, afritaði föll úr projecti
    
    Build tími fór úr ~55s í ~5s!

--- src/lib/FileProcessor.js
+++ src/lib/FileProcessor.js
@@ -2,7 +2,7 @@ const util = require('util');
 const fs = require('fs');
 const path = require('path');
 
-const marked = require('marked');
+const {marked} = require('marked');
 const graymatter = require('gray-matter');
 const jsdom = require('jsdom');
 
