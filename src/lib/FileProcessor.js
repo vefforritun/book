a4cdbb575687ec1269a5e288952d0b8e4720d24f
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2023-04-16 14:59:33 +0000

    fix: prettier & import order on `FileProcessor`

--- src/lib/FileProcessor.js
+++ src/lib/FileProcessor.js
@@ -1,63 +1,68 @@
-const util = require('util');
-const fs = require('fs');
-const path = require('path');
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
 
-const {marked} = require('marked');
-const graymatter = require('gray-matter');
-const jsdom = require('jsdom');
+const { marked } = require("marked");
+const graymatter = require("gray-matter");
+const jsdom = require("jsdom");
 
-const Renderer = require('./Renderer');
-const { autolink } = require('./utils');
-const EstimateReadingTime = require('./estimateReadingTime');
+const Renderer = require("./Renderer");
+const { autolink } = require("./utils");
+const EstimateReadingTime = require("./estimateReadingTime");
 
 const readFileAsync = util.promisify(fs.readFile);
 
 const WORDS_PER_MINUTE = 200;
 
-const readingTimeEstimator = new EstimateReadingTime({ words_per_minute: WORDS_PER_MINUTE });
+const readingTimeEstimator = new EstimateReadingTime({
+  words_per_minute: WORDS_PER_MINUTE,
+});
 
 const abbreviations = {
-  'TCP': 'Transmission Control Protocol',
-  'SMTP': 'Simple Mail Transfer Protocol',
-  'FTP': 'File Transfer Protocol',
-  'URL': 'Uniform Resource Locator',
-  'HTTP': 'HyperText Transfer Protocol',
-  'IPv6': 'Internet Protocol, version 6',
-  'IPv4': 'Internet Protocol, version 4',
-  'IP': 'Internet Protocol',
-  'DNS': 'Domain Name System',
-  'HTML': 'HyperText Markup Language',
-  'CSS': 'Cascading Style Sheets',
-  'WYSIWYG': 'What You See Is What You Get',
-  'CLI': 'Command Line Interface',
-  'DOM': 'Document Object Model',
-  'GUI': 'Graphical User Interface',
+  TCP: "Transmission Control Protocol",
+  SMTP: "Simple Mail Transfer Protocol",
+  FTP: "File Transfer Protocol",
+  URL: "Uniform Resource Locator",
+  HTTP: "HyperText Transfer Protocol",
+  IPv6: "Internet Protocol, version 6",
+  IPv4: "Internet Protocol, version 4",
+  IP: "Internet Protocol",
+  DNS: "Domain Name System",
+  HTML: "HyperText Markup Language",
+  CSS: "Cascading Style Sheets",
+  WYSIWYG: "What You See Is What You Get",
+  CLI: "Command Line Interface",
+  DOM: "Document Object Model",
+  GUI: "Graphical User Interface",
 };
 
 function postprocess(html) {
   const dom = new jsdom.JSDOM(html);
 
   // inline footnotes
-  dom.window.document.querySelectorAll('sup.footnote-mark').forEach(i => {
+  dom.window.document.querySelectorAll("sup.footnote-mark").forEach((i) => {
     const number = i.dataset.number;
-    const textElement = dom.window.document.querySelector(`sup.footnote-text[data-number="${number}"]`);
+    const textElement = dom.window.document.querySelector(
+      `sup.footnote-text[data-number="${number}"]`
+    );
 
     if (textElement) {
       const footnote = textElement.parentNode;
-      i.insertAdjacentElement('afterend', footnote);
+      i.insertAdjacentElement("afterend", footnote);
     } else {
-      console.warn('unable to find footnote text');
+      console.warn("unable to find footnote text");
     }
   });
 
   // mark abbrs
   const seen = [];
-  const skippedNodes = ['h1', 'h2', 'h3'];
+  const skippedNodes = ["h1", "h2", "h3"];
   const walk = (node) => {
-    const {
-      childNodes, nodeType, nodeName,textContent: text,
-    } = node;
-    if (nodeType === node.ELEMENT_NODE && skippedNodes.indexOf(nodeName.toLowerCase()) < 0) {
+    const { childNodes, nodeType, nodeName, textContent: text } = node;
+    if (
+      nodeType === node.ELEMENT_NODE &&
+      skippedNodes.indexOf(nodeName.toLowerCase()) < 0
+    ) {
       for (const child of Array.from(childNodes)) {
         walk(child);
       }
@@ -66,7 +71,7 @@ function postprocess(html) {
       const toReplaceForCurrentNode = [];
 
       for (const abbr in abbreviations) {
-        const regexp = new RegExp(`${abbr}`, 'g');
+        const regexp = new RegExp(`${abbr}`, "g");
         if (text.match(regexp) && seen.indexOf(abbr) < 0) {
           toReplaceForCurrentNode.push(abbr);
           seen.push(abbr);
@@ -75,17 +80,18 @@ function postprocess(html) {
 
       // replace all
       if (toReplaceForCurrentNode.length > 0) {
-        let replaced = text
+        let replaced = text;
 
         toReplaceForCurrentNode.forEach((i) => {
           const replacement = `<abbr title="${abbreviations[i]}">${i}</abbr>`;
-          const regexp = new RegExp(`${i}`, 'g');
+          const regexp = new RegExp(`${i}`, "g");
           replaced = replaced.replace(regexp, replacement);
-        })
+        });
 
         // create a new jsdom document that creates a "graft" for us to replace the text with—with actual HTML
         // because node.innerHTML doesn't work..?
-        const graft = new jsdom.JSDOM('<span>'+replaced+'</span>').window.document.body.firstChild;
+        const graft = new jsdom.JSDOM("<span>" + replaced + "</span>").window
+          .document.body.firstChild;
 
         const parent = node.parentNode;
 
@@ -95,7 +101,7 @@ function postprocess(html) {
         }
       }
     }
-  }
+  };
 
   walk(dom.window.document.body);
 
@@ -104,12 +110,12 @@ function postprocess(html) {
 
 function markedSingleLine(content) {
   if (!content) {
-    return '';
+    return "";
   }
 
-  const replaced = content.replace(/\\\[/g, '[');
+  const replaced = content.replace(/\\\[/g, "[");
 
-  const output = marked(replaced).replace('<div class="paragraphs">', '');
+  const output = marked(replaced).replace('<div class="paragraphs">', "");
 
   return output;
 }
@@ -123,12 +129,12 @@ module.exports = class FileProcessor {
 
   makeNextPrevContent(content) {
     if (!content) {
-      return '';
+      return "";
     }
 
-    const replaced = content.replace(/\\\[/g, '[');
+    const replaced = content.replace(/\\\[/g, "[");
 
-    const output = marked(replaced).replace('<div class="paragraphs">', '');
+    const output = marked(replaced).replace('<div class="paragraphs">', "");
 
     return output;
   }
@@ -143,29 +149,39 @@ module.exports = class FileProcessor {
     const {
       content,
       data: {
-        title = '',
+        title = "",
         chapter: metaChapter = 0,
-        version = '',
+        version = "",
         history = [],
-        next = '',
-        previous = '',
+        next = "",
+        previous = "",
       } = {},
     } = graymatter(fileContent);
 
-    const chapter = isNaN(parseInt(metaChapter, 10)) ? 1 : parseInt(metaChapter, 10);
-    const renderer = new Renderer({ chapter, basedir, reporter: this.reporter });
+    const chapter = isNaN(parseInt(metaChapter, 10))
+      ? 1
+      : parseInt(metaChapter, 10);
+    const renderer = new Renderer({
+      chapter,
+      basedir,
+      reporter: this.reporter,
+    });
 
     marked.setOptions({
       renderer,
     });
 
-    const renderedHtml = renderer.postRender(marked(content.toString(this.encoding)));
+    const renderedHtml = renderer.postRender(
+      marked(content.toString(this.encoding))
+    );
 
     const html = postprocess(renderedHtml);
 
     const previousContent = this.makeNextPrevContent(previous);
     const nextContent = this.makeNextPrevContent(next);
-    const estimatedReadingTime = readingTimeEstimator.estimate(content.toString(this.encoding));
+    const estimatedReadingTime = readingTimeEstimator.estimate(
+      content.toString(this.encoding)
+    );
     const historyLinked = history.map((i) => markedSingleLine(i));
 
     return {
@@ -182,4 +198,4 @@ module.exports = class FileProcessor {
       content: html,
     };
   }
-}
+};
